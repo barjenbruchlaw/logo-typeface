07d6868ba95b81bd0c5960b3572267278342307d
barjenbruchlaw <brian@kcpropertylaw.com>
2022-09-15 10:21:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="description">
+    <h1>Fonts for new logo</h1>
+    <h4>
+      <p>Below please find several options for the font for the new logo.</p>
+      <p>Please choose 3 options in the Survey Monkey survey.</p>
+      <p>
+        Also, if you have any fonts you would like to see as an option here,
+        please let me know.
+      </p>
+    </h4>
+  </div>
+  <hr />
+  <div className="options">
+    <div className="option1"><p>1. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option2"><p>2. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option3"><p>3. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option4"><p>4. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option5"><p>5. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option6"><p>6. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option7"><p>7. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option8"><p>8. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option9"><p>9. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option10"><p>10. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option11"><p>11. Bassin, Young & Barjenbruch, LLC</p></div>
+    <div className="option12"><p>12. Bassin, Young & Barjenbruch, LLC</p></div>
+  </div>
     </div>
   );
 }
